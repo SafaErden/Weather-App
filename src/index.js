8c84a68732ea1e42ebd810422f73f797b4f77ba1
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-09-02 22:22:53 +0300

    set initial configuration for webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+	const element = document.createElement('div');
+
+	// Lodash, currently included via a script, is required for this line to work
+	element.innerHTML = _.join([ 'You are', 'too hot ;)' ], ' ');
+	element.classList.add('hello');
+
+	return element;
+}
+
+document.body.appendChild(component());
