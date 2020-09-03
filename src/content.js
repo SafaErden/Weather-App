1d8472f8372bd3cb78153c5759dc537ecd39167e
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-09-03 18:49:19 +0300

    add icons

--- src/content.js
+++ src/content.js
@@ -1,16 +1,42 @@
 function populateContent(params) {
 	Promise.resolve(params).then(function(value) {
-		console.log(value); // "Success"
 		const mainHeader = document.getElementById('mainHeader');
 		const mainDegree = document.getElementById('mainDegree');
-		const mainGif = document.getElementById('mainGif');
 		const mainInfo = document.getElementById('mainInfo');
-		const Date = document.getElementById('Date');
 		mainHeader.innerText = value['name'];
-		mainDegree.innerText = value['main']['temp'];
-		mainGif.innerText = value['weather'][0]['icon'];
-		mainInfo.innerText = value['weather'][0]['description'];
-		Date.innerText = value['dt'];
+		mainDegree.innerText = value['main']['temp'] + ' Celcius';
+		mainInfo.innerText =
+			value['weather'][0]['description'].charAt(0).toUpperCase() + value['weather'][0]['description'].slice(1);
+
+		const wind = document.getElementById('wind');
+		const humid = document.getElementById('humid');
+		const cloud = document.getElementById('cloud');
+		const pressure = document.getElementById('pressure');
+
+		wind.innerText = value['wind']['speed'] + ' KNOTS';
+		humid.innerText = value['main']['humidity'] + ' %';
+		cloud.innerText = value['clouds']['all'] + ' %';
+		pressure.innerText = value['main']['pressure'] + ' mBar';
+
+		const mainIcon = document.getElementById('mainIcon');
+
+		switch (value['weather'][0]['main']) {
+			case 'Clouds':
+				mainIcon.className = '';
+				mainIcon.classList.add('fas', 'fa-cloud', 'text-gray', 'display-1');
+				break;
+			case 'Rain':
+				mainIcon.className = '';
+				mainIcon.classList.add('fas', 'fa-cloud-rain', 'text-primary', 'display-1');
+				break;
+			case 'Clear':
+				mainIcon.className = '';
+				mainIcon.classList.add('fas', 'fa-sun', 'text-warning', 'display-1');
+				break;
+			default:
+				mainIcon.className = '';
+				mainIcon.classList.add('fas', 'fa-cloud-sun', 'text-warning', 'display-1');
+		}
 	});
 }
 
--- src/search.js
+++ src/search.js
@@ -1,6 +1,9 @@
 import getCities from './cities';
 import populateContent from './content';
 
+const listWrapper = document.querySelector('.listWrapper');
+const results = document.createElement('ul');
+
 function resetResults(listWrapper, results) {
 	listWrapper.innerHTML = '';
 	results.innerHTML = '';
@@ -11,20 +14,18 @@ function listenResults() {
 	results.forEach((item) => {
 		item.addEventListener('click', () => {
 			const finalResult = getLocations(item.innerText);
+			resetResults(listWrapper, results);
 			populateContent(finalResult);
 		});
 	});
 }
 
 async function manageSeacrh() {
-	const listWrapper = document.querySelector('.listWrapper');
-
-	const results = document.createElement('ul');
 	results.classList.add('list-unstyled', 'p-0', 'm-0');
 
 	const search = document.getElementsByTagName('input')[0];
 	search.addEventListener('input', async () => {
-		let searchResult = await getCities(search.value);
+		let searchResult = await getCities(search.value.charAt(0).toUpperCase() + search.value.slice(1));
 		resetResults(listWrapper, results);
 		searchResult.forEach((item) => {
 			const result = document.createElement('li');
@@ -40,7 +41,9 @@ async function manageSeacrh() {
 
 async function getLocations(value) {
 	const response = await fetch(
-		'http://api.openweathermap.org/data/2.5/weather?q=' + value + '&APPID=a62a939fdc5a7df5164d021d0e94ce6b',
+		'http://api.openweathermap.org/data/2.5/weather?q=' +
+			value +
+			'&APPID=a62a939fdc5a7df5164d021d0e94ce6b&units=metric',
 		{ mode: 'cors' }
 	);
 	const result = await response.json();
