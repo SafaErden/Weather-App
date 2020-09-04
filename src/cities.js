23165b7f084518564af48776fc6883436e2328e2
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-09-04 11:33:14 +0300

    fix toggle button

--- src/cities.js
+++ src/cities.js
@@ -1,24 +1,26 @@
 async function filterCities(json, param) {
-  let i = 0;
-  let j = 0;
-  const firstFive = [];
-  while (j < 5 && i < json.length) {
-    if (json[i].name.includes(param)) {
-      firstFive.push(json[i].name);
-      j += 1;
-    }
-    i += 1;
-  }
-  return firstFive;
+	let i = 0;
+	let j = 0;
+	const firstFive = [];
+	while (j < 5 && i < json.length) {
+		if (json[i].name.includes(param)) {
+			firstFive.push(json[i].name);
+			j += 1;
+		}
+		i += 1;
+	}
+	return firstFive;
 }
 
 async function getCities(param) {
-  const response = await fetch('../src/city.list.json');
-  const cities = await response.json();
-
-  const filterCity = await filterCities(cities, param);
-
-  return filterCity;
+	try {
+		const response = await fetch('../src/city.list.json');
+		const cities = await response.json();
+		const filterCity = await filterCities(cities, param);
+		return filterCity;
+	} catch (e) {
+		alert('Opps, something went wrong!');
+	}
 }
 
 export default getCities;
--- src/search.js
+++ src/search.js
@@ -5,49 +5,53 @@ const listWrapper = document.querySelector('.listWrapper');
 const results = document.createElement('ul');
 
 async function getLocations(value) {
-  const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=a62a939fdc5a7df5164d021d0e94ce6b&units=metric`,
-    { mode: 'cors' },
-  );
-  const result = await response.json();
-  return result;
+	try {
+		const response = await fetch(
+			`http://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=a62a939fdc5a7df5164d021d0e94ce6b&units=metric`,
+			{ mode: 'cors' }
+		);
+		const result = await response.json();
+		return result;
+	} catch (e) {
+		alert(e, 'Oops, something went wrong!');
+	}
 }
 
 function resetResults(listWrapper, results) {
-  listWrapper.innerHTML = '';
-  results.innerHTML = '';
+	listWrapper.innerHTML = '';
+	results.innerHTML = '';
 }
 
 function listenResults() {
-  const results = document.querySelectorAll('.searchItems');
-  results.forEach((item) => {
-    item.addEventListener('click', () => {
-      const finalResult = getLocations(item.innerText);
-      resetResults(listWrapper, results);
-      populateContent(finalResult);
-    });
-  });
+	const results = document.querySelectorAll('.searchItems');
+	results.forEach((item) => {
+		item.addEventListener('click', () => {
+			const finalResult = getLocations(item.innerText);
+			resetResults(listWrapper, results);
+			populateContent(finalResult);
+		});
+	});
 }
 
 async function manageSeacrh() {
-  results.classList.add('list-unstyled', 'p-0', 'm-0');
+	results.classList.add('list-unstyled', 'p-0', 'm-0');
 
-  const search = document.getElementsByTagName('input')[0];
-  search.addEventListener('input', async () => {
-    const searchResult = await getCities(
-      search.value.charAt(0).toUpperCase() + search.value.slice(1),
+	const search = document.getElementsByTagName('input')[0];
+	search.addEventListener('input', async () => {
+		const searchResult = await getCities(
+			search.value.charAt(0).toUpperCase() + search.value.slice(1)
 		); /* eslint-disable-line */
-    resetResults(listWrapper, results);
-    searchResult.forEach((item) => {
-      const result = document.createElement('li');
-      result.classList.add('border', 'p-2', 'm-0', 'searchItems');
-      result.innerText = item;
-      results.appendChild(result);
-    });
-    if (listWrapper.appendChild(results)) {
-      listenResults();
-    }
-  });
+		resetResults(listWrapper, results);
+		searchResult.forEach((item) => {
+			const result = document.createElement('li');
+			result.classList.add('border', 'p-2', 'm-0', 'searchItems');
+			result.innerText = item;
+			results.appendChild(result);
+		});
+		if (listWrapper.appendChild(results)) {
+			listenResults();
+		}
+	});
 }
 
 export { manageSeacrh, listenResults };
